5576aa1ff0175b4b47f323a077588e75acf2b6d1
Andras Nemeseri <andris@nemeseri.com>
2012-03-16 05:09:08 +0100

    display fix for future css transitions

--- ender.overlay.js
+++ ender.overlay.js
@@ -243,7 +243,9 @@
 			};
 
 			this.setOptions(options);
-			this.$overlay = $el;
+			this.$overlay = $el.css({
+				display: "none"
+			});
 
 			if (this.options.showMask) {
 				this.initMask();
